8529c309fae3e71a2ceb90fb45b8a9091ba3f09c
c-roos <chrisroos1313@gmail.com>
2019-12-03 13:06:11 -0600

    testing

--- scrabble.js
+++ scrabble.js
@@ -1,5 +1,5 @@
 async function getWords() {
-    const response = await fetch('/words.txt');
+    const response = await fetch('https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt');
     const myText = await response.text();
     console.log(myText);
 }
