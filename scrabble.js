766ac9f43c1e587d4130fc821481c0056ff8cfe1
c-roos <chrisroos1313@gmail.com>
2019-12-03 13:34:55 -0600

    testing

--- scrabble.js
+++ scrabble.js
@@ -1,7 +1,3 @@
-async function getWords() {
-    const response = await fetch('https://raw.githubusercontent.com/dwyl/english-words/master/words_alpha.txt');
-    const myText = await response.text();
-    console.log(myText);
-}
+import getWords from './asyncmodule.mjs';
 
-getWords();
+console.log(getWords());
