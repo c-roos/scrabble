47780c11213e419f365f234a50be52ea94fce61a
c-roos <chrisroos1313@gmail.com>
2019-12-03 14:06:57 -0600

    testing

--- scrabble.js
+++ scrabble.js
@@ -2,8 +2,9 @@ import { getWords } from './asyncmodule.js';
 
 var words;
 
-getWords().then(function (t) {
-  words = t;
+getWords().then(function (value) {
+    console.log(value);
+    words = value;
 });
 
 console.log(words);
