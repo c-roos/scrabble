99bd21401b4b3cb2300505fe00bdffe08affa0c1
c-roos <chrisroos1313@gmail.com>
2019-12-04 09:34:29 -0600

    testing

--- scrabble.js
+++ scrabble.js
@@ -1,22 +1,57 @@
 import { getWords } from './asyncmodule.js';
 
+var scoreMap = {
+    'a': 1,
+    'b': 3,
+    'c': 3,
+    'd': 2,
+    'e': 1,
+    'f': 4,
+    'g': 2,
+    'h': 4,
+    'i': 1,
+    'j': 8,
+    'k': 5,
+    'l': 1,
+    'm': 3,
+    'n': 1,
+    'o': 1,
+    'p': 3,
+    'q': 10,
+    'r': 1,
+    's': 1,
+    't': 1,
+    'u': 1,
+    'v': 4,
+    'w': 4,
+    'x': 8,
+    'y': 4,
+    'z': 10
+};
+
 var englishWords = null;
+
+function calcScore(word) {
+    var score = 0;
+    for (i = 0; i < word.length; i++) {
+        score += scoreMap[word.charAt(i)];
+    }
+    return score;
+}
 
 function calcWords(presuf, words) {
     var t = Date.now();
-    //console.log(words[0].charCodeAt(0));
-    //console.log(words[0].charCodeAt(1));
     var contains = [];
     words.forEach(function (item) {
         if (item.startsWith(presuf) || item.endsWith(presuf)) {
             contains.push(item);
         }
-        //if (item === presuf) {
-        //    contains.push(item);
-        //}
     });
     console.log((Date.now() - t) / 1000);
-    console.log(contains);
+    //console.log(contains);
+    contains.foreach(function (word) {
+        return console.log(word + ': ' + calcScore(word));
+    });
 }
 
 function formHandler(e) {
